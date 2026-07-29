affcba1aacde163d98612861d3117ec577930ecc
Alexander <46108494+TeutonStudio@users.noreply.github.com>
2026-07-29 08:21:18 +0200

    v2.3.2: neue Operatorauswerter registrieren

--- MathematikKnoten/src/main/kotlin/de/TeutonStudio/MathematikKnoten/geometrie/GesamterMathematikAuswerter.kt
+++ MathematikKnoten/src/main/kotlin/de/TeutonStudio/MathematikKnoten/geometrie/GesamterMathematikAuswerter.kt
@@ -2,9 +2,10 @@ package de.TeutonStudio.MathematikKnoten
 
 import de.TeutonStudio.MathematikKartenAdapter.MathematikAuswerterRegister
 
-/** Bestehender v2.1.18-Auswerter plus additive Geometriedomäne. */
+/** Bestehender Standardauswerter plus additive Geometriedomäne und versionierte Knotenerweiterungen. */
 object GesamterMathematikAuswerter {
     fun erzeugeRegister(): MathematikAuswerterRegister = StandardMathematikAuswerter.erzeugeRegister().apply {
+        registriereDivisionUndKehrwert()
         registriereGeometrieGrundobjekte()
         registriereGeometrieRelationen()
         registriereGeometrieTransformationen()
