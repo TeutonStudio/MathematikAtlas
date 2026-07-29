3c3e3bb32749826601dfb897a2a65708aa1b4159
Alexander <46108494+TeutonStudio@users.noreply.github.com>
2026-07-29 17:05:29 +0200

    App-Einstieg an Darstellungsmodus anbinden

--- app/src/main/kotlin/de/TeutonStudio/MathematikAtlas/MainActivity.kt
+++ app/src/main/kotlin/de/TeutonStudio/MathematikAtlas/MainActivity.kt
@@ -3,16 +3,26 @@ package de.TeutonStudio.MathematikAtlas
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MaterialTheme(colorScheme = lightColorScheme()) {
-                val zustand = remember { AtlasZustand(applicationContext) }
+            val darstellungsSpeicher = remember { DarstellungsEinstellungenSpeicher(applicationContext) }
+            var darstellungsModus by remember { mutableStateOf(darstellungsSpeicher.lade()) }
+            val zustand = remember { AtlasZustand(applicationContext) }
+
+            MathematikAtlasTheme(
+                modus = darstellungsModus,
+                onModusÄndern = { modus ->
+                    darstellungsModus = modus
+                    darstellungsSpeicher.speichere(modus)
+                },
+            ) {
                 MathematikAtlasApp(zustand)
             }
         }
