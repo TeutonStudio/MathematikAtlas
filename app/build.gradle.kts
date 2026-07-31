ac2cc9160b7f442e90c88ade35f6229a79257c20
Alexander <46108494+TeutonStudio@users.noreply.github.com>
2026-07-31 11:14:43 +0200

    v2.6.0: Android-Version aktualisieren

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "de.TeutonStudio.MathematikAtlas"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2005002
-        versionName = "2.5.2"
+        versionCode = 2006000
+        versionName = "2.6.0"
     }
 
     buildFeatures { compose = true }
