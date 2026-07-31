62a3cccd4b7bcba7c1aa75ac72e91bc82241e546
Alexander <46108494+TeutonStudio@users.noreply.github.com>
2026-07-31 15:55:59 +0200

    Registriert Aussagenlogik auf v2.9.1

--- MathematikKnoten/src/main/kotlin/de/TeutonStudio/MathematikKnoten/geometrie/GesamterMathematikAuswerter.kt
+++ MathematikKnoten/src/main/kotlin/de/TeutonStudio/MathematikKnoten/geometrie/GesamterMathematikAuswerter.kt
@@ -2,12 +2,13 @@ package de.TeutonStudio.MathematikKnoten
 
 import de.TeutonStudio.MathematikKartenAdapter.MathematikAuswerterRegister
 
-/** Bestehender Standardauswerter plus additive Geometrie- und Mengenraumdomänen. */
+/** Bestehender Standardauswerter plus additive Geometrie-, Mengenraum- und Aussagenlogikdomänen. */
 object GesamterMathematikAuswerter {
     fun erzeugeRegister(): MathematikAuswerterRegister = StandardMathematikAuswerter.erzeugeRegister().apply {
         registriereGeometrieGrundobjekte()
         registriereGeometrieRelationen()
         registriereGeometrieTransformationen()
         registriereMengenraumKnoten()
+        registriereAussagenLogikKnoten()
     }
 }
