fffd4f3d27e3d7a7092aacb37e4aa213cb1b0625
Alexander <46108494+TeutonStudio@users.noreply.github.com>
2026-07-31 11:29:27 +0200

    Version 2.5.3 setzen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "de.TeutonStudio.MathematikAtlas"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2005002
-        versionName = "2.5.2"
+        versionCode = 2005003
+        versionName = "2.5.3"
     }
 
     buildFeatures { compose = true }
