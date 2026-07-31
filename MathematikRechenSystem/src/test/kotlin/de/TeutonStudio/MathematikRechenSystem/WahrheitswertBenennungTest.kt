c4e3b01b5baa4e2e4f7b6699db829db105fa3ed3
Alexander <46108494+TeutonStudio@users.noreply.github.com>
2026-07-31 16:35:59 +0200

    Fixiert universelle Wahrheitswert-LaTeX-Symbole

--- MathematikRechenSystem/src/test/kotlin/de/TeutonStudio/MathematikRechenSystem/WahrheitswertBenennungTest.kt
+++ MathematikRechenSystem/src/test/kotlin/de/TeutonStudio/MathematikRechenSystem/WahrheitswertBenennungTest.kt
@@ -8,8 +8,12 @@ class WahrheitswertBenennungTest {
     @Test
     fun wahrheitswerteHeißenAusschließlichWahrUndLüge() {
         assertEquals(listOf("Wahr", "Lüge"), Wahrheitswert.entries.map { it.name })
+        assertEquals("\\mathcal{Wahr}", Wahrheitswert.Wahr.latex)
+        assertEquals("\\mathcal{Lüge}", Wahrheitswert.Lüge.latex)
         assertEquals(Wahrheitswert.Wahr, WahrheitsKonstante(true).entscheide().wahrheitswert)
         assertEquals(Wahrheitswert.Lüge, WahrheitsKonstante(false).entscheide().wahrheitswert)
+        assertEquals(Wahrheitswert.Wahr.latex, WahrheitsKonstante(true).zuLatex())
+        assertEquals(Wahrheitswert.Lüge.latex, WahrheitsKonstante(false).zuLatex())
     }
 
     @Test
