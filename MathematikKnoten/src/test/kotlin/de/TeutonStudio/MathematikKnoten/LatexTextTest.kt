baa909494a3c4c7932c422d4b4b43bd08fe14bd4
Alexander <46108494+TeutonStudio@users.noreply.github.com>
2026-08-01 15:41:31 +0200

    Test auf ausgefülltes Adjunktionssymbol aktualisieren

--- MathematikKnoten/src/test/kotlin/de/TeutonStudio/MathematikKnoten/LatexTextTest.kt
+++ MathematikKnoten/src/test/kotlin/de/TeutonStudio/MathematikKnoten/LatexTextTest.kt
@@ -29,9 +29,9 @@ class LatexTextTest {
 
     @Test
     fun `rendert große und gepunktete Aussagenoperatoren`() {
-        assertEquals("a ∨̊ b", vereinfacheLatexAnzeige("a \\stackrel{\\circ}{\\lor} b"))
+        assertEquals("a ∨̇ b", vereinfacheLatexAnzeige("a \\stackrel{\\circ}{\\lor} b"))
         assertEquals(
-            "⋁̊idx ∈ {A} methode(idx)",
+            "⋁̇idx ∈ {A} methode(idx)",
             vereinfacheLatexAnzeige("\\stackrel{\\circ}{\\bigvee}_{idx \\in \\Set{A}} methode(idx)"),
         )
         assertEquals("⋀idx ∈ {A} P(idx)", vereinfacheLatexAnzeige("\\bigwedge_{idx \\in \\Set{A}} P(idx)"))
