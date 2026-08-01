e8bdf05eee2c137d4664ccd28ec1f3f31c7b667f
Alexander <46108494+TeutonStudio@users.noreply.github.com>
2026-08-01 23:44:56 +0200

    v2.13.1
    Methodenaufruf aus dem verbundenen Methodenvertrag ableiten
    
    - Stelligkeit und Parameterreihenfolge automatisch übernehmen
    - Argumentanschlüsse deterministisch erzeugen und bestehende Kanten migrieren
    - Argument- und Ausgangsarten sowie Wertebereiche und Zielmenge ableiten
    - Vertragsdaten schreibgeschützt im Inspector anzeigen
    - ungültig gewordene Kanten kontrolliert bereinigen
    - Struktur-, Typ- und Persistenztests ergänzen
    
    Closes #71

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "de.TeutonStudio.MathematikAtlas"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2013000
-        versionName = "2.13.0"
+        versionCode = 2013001
+        versionName = "2.13.1"
     }
 
     buildFeatures { compose = true }
