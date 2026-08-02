dd2f6366a3fe33b4d086ede8509d16c89b7a3ee1
Alexander <46108494+TeutonStudio@users.noreply.github.com>
2026-08-02 23:04:03 +0200

    v2.17.1
    Verbindungsinteraktion präzisieren
    
    - Bézier-Geometrie für Zeichnung, Hittest und Culling vereinheitlichen
    - zoombeständige Trefferbereiche und Mindeststrichbreiten ergänzen
    - Pointer-Drag auf absolute Positionen umstellen
    - magnetisches Einrasten mit Hysterese und Zielhervorhebung implementieren
    - Geometrietests für Kurven- und Randfälle ergänzen
    - Android-Version und Releaseplan auf 2.17.1 aktualisieren
    
    Fixes #85

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "de.TeutonStudio.MathematikAtlas"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2017000
-        versionName = "2.17.0"
+        versionCode = 2017001
+        versionName = "2.17.1"
     }
 
     buildFeatures { compose = true }
