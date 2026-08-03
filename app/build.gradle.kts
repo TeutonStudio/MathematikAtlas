a7099a2559ecf4438b764b335f87694d0e40fdf9
Alexander <46108494+TeutonStudio@users.noreply.github.com>
2026-08-04 01:08:27 +0200

    Android-Version auf 2.23.0 aktualisieren

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,8 +36,8 @@ android {
         applicationId = "de.TeutonStudio.MathematikAtlas"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2022001
-        versionName = "2.22.1"
+        versionCode = 2023000
+        versionName = "2.23.0"
         buildConfigField("String", "GIT_COMMIT_SHA", "\"$gitCommitSha\"")
     }
 
