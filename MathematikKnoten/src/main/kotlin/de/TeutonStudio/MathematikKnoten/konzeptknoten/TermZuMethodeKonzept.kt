ce35ff556e4042d14d176bdb49010ae3590febd9
Alexander <46108494+TeutonStudio@users.noreply.github.com>
2026-08-04 21:26:04 +0200

    Issue 261: Term-zu-Methode-Konzept definieren

---
+++ MathematikKnoten/src/main/kotlin/de/TeutonStudio/MathematikKnoten/konzeptknoten/TermZuMethodeKonzept.kt
@@ -0,0 +1,22 @@
+package de.TeutonStudio.MathematikKnoten.konzeptknoten
+
+import de.TeutonStudio.KnotenKartenVerwalter.daten.KnotenVorlage
+import de.TeutonStudio.MathematikKnoten.enzyklopädie.WissensEintrag
+import de.TeutonStudio.MathematikKnoten.enzyklopädie.WissensId
+
+object TermZuMethodeKonzept {
+    val id = WissensId("konzept.term-zu-methode")
+
+    fun passt(vorlage: KnotenVorlage): Boolean =
+        "termzumethode" in vorlage.art.lowercase() ||
+            "term zu methode" in vorlage.name.lowercase()
+
+    fun erstelle(vorlagen: List<KnotenVorlage>): WissensEintrag = gruppiertesVorlagenKonzept(
+        id = id,
+        titel = "Term zu Methode",
+        beschreibung = "Bindet freie Parameter eines Terms und stellt den Term als typisierte Methode mit Zielmenge bereit.",
+        vorlagen = vorlagen.filter(::passt),
+        generatorId = "konzeptkarte.term-zu-methode",
+        zusätzlicheSuchbegriffe = setOf("Methode", "Abbildung", "Parameterbindung", "Lambda", "Term"),
+    )
+}
