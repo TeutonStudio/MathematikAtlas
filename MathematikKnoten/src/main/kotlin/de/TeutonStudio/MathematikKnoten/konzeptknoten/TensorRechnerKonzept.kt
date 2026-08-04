30532058ffcd0838cca98983efe95db2b19b33e3
Alexander <46108494+TeutonStudio@users.noreply.github.com>
2026-08-04 21:25:23 +0200

    Issue 261: Tensorrechner-Konzept bündeln

---
+++ MathematikKnoten/src/main/kotlin/de/TeutonStudio/MathematikKnoten/konzeptknoten/TensorRechnerKonzept.kt
@@ -0,0 +1,21 @@
+package de.TeutonStudio.MathematikKnoten.konzeptknoten
+
+import de.TeutonStudio.KnotenKartenVerwalter.daten.KnotenVorlage
+import de.TeutonStudio.MathematikKnoten.enzyklopädie.WissensEintrag
+import de.TeutonStudio.MathematikKnoten.enzyklopädie.WissensId
+import de.TeutonStudio.MathematikRechenSystem.kern.TensorRechner
+
+object TensorRechnerKonzept {
+    val id = WissensId("konzept.tensorrechner")
+
+    fun passt(vorlage: KnotenVorlage): Boolean = vorlage.art == TensorRechner.KNOTEN_ART
+
+    fun erstelle(vorlagen: List<KnotenVorlage>): WissensEintrag = gruppiertesVorlagenKonzept(
+        id = id,
+        titel = "Tensorrechner",
+        beschreibung = "Universeller Rechner für komponentenweise, indexbezogene und formverändernde Tensoroperationen.",
+        vorlagen = vorlagen.filter(::passt),
+        generatorId = "konzeptkarte.tensorrechner",
+        zusätzlicheSuchbegriffe = setOf("Tensor", "Matrix", "Vektor", "Kontraktion", "Achsenpermutation"),
+    )
+}
