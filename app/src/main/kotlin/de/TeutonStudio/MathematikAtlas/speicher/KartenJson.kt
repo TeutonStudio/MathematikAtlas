fb51cfa4c7d135048a355fe3fea1233f7db94729
Alexander <46108494+TeutonStudio@users.noreply.github.com>
2026-08-07 00:23:26 +0200

    feat(kern): migriere Methodeneinschraenkung beim Karten-JSON

--- app/src/main/kotlin/de/TeutonStudio/MathematikAtlas/speicher/KartenJson.kt
+++ app/src/main/kotlin/de/TeutonStudio/MathematikAtlas/speicher/KartenJson.kt
@@ -3,14 +3,17 @@ package de.TeutonStudio.MathematikAtlas.speicher
 import de.TeutonStudio.KnotenKartenVerwalter.daten.KartenDaten
 import de.TeutonStudio.KnotenKartenVerwalter.daten.KartenDatenJson
 import de.TeutonStudio.MathematikAtlas.migriereTranspositionsKnoten
+import de.TeutonStudio.MathematikKnoten.migriereMethodenEinschraenkungKnoten
 import org.json.JSONObject
 
 /** App-Fassade: reiner Codec im Verwalter, App-spezifische Migration anschließend. */
 object KartenJson {
-    fun schreibe(karte: KartenDaten): String = KartenDatenJson.schreibe(karte)
+    fun schreibe(karte: KartenDaten): String =
+        KartenDatenJson.schreibe(karte.migriereMethodenEinschraenkungKnoten())
 
     fun lese(text: String): KartenDaten = lese(JSONObject(text))
 
-    fun lese(json: JSONObject): KartenDaten =
-        KartenDatenJson.lese(json).let(::migriereTranspositionsKnoten)
+    fun lese(json: JSONObject): KartenDaten = KartenDatenJson.lese(json)
+        .let(::migriereTranspositionsKnoten)
+        .migriereMethodenEinschraenkungKnoten()
 }
