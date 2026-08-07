dc620f784909207909750f81f0b3f20f12d623ae
Alexander <46108494+TeutonStudio@users.noreply.github.com>
2026-08-07 12:11:31 +0200

    Replay #309: Hyper- und Tensor-Migration kombinieren

--- app/src/main/kotlin/de/TeutonStudio/MathematikAtlas/speicher/KartenJson.kt
+++ app/src/main/kotlin/de/TeutonStudio/MathematikAtlas/speicher/KartenJson.kt
@@ -3,13 +3,14 @@ package de.TeutonStudio.MathematikAtlas.speicher
 import de.TeutonStudio.KnotenKartenVerwalter.daten.KartenDaten
 import de.TeutonStudio.KnotenKartenVerwalter.daten.KartenDatenJson
 import de.TeutonStudio.MathematikAtlas.migriereTranspositionsKnoten
+import de.TeutonStudio.MathematikKnoten.migriereHyperAnalysisKnoten
 import de.TeutonStudio.MathematikKnoten.migriereTensorOperationKnoten
 import org.json.JSONObject
 
 /** App-Fassade: reiner Codec im Verwalter, App-spezifische Migration anschließend. */
 object KartenJson {
     fun schreibe(karte: KartenDaten): String = KartenDatenJson.schreibe(
-        karte.migriereTensorOperationKnoten(),
+        karte.migriereTensorOperationKnoten().migriereHyperAnalysisKnoten(),
     )
 
     fun lese(text: String): KartenDaten = lese(JSONObject(text))
@@ -17,4 +18,5 @@ object KartenJson {
     fun lese(json: JSONObject): KartenDaten = KartenDatenJson.lese(json)
         .let(::migriereTranspositionsKnoten)
         .migriereTensorOperationKnoten()
+        .migriereHyperAnalysisKnoten()
 }
