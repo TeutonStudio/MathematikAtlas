401982484a7f4962adaf47abbed249d0a9bd33d4
Alexander <46108494+TeutonStudio@users.noreply.github.com>
2026-08-07 12:02:02 +0200

    Replay #308: Tensor-Migration in KartenJson

--- app/src/main/kotlin/de/TeutonStudio/MathematikAtlas/speicher/KartenJson.kt
+++ app/src/main/kotlin/de/TeutonStudio/MathematikAtlas/speicher/KartenJson.kt
@@ -3,14 +3,18 @@ package de.TeutonStudio.MathematikAtlas.speicher
 import de.TeutonStudio.KnotenKartenVerwalter.daten.KartenDaten
 import de.TeutonStudio.KnotenKartenVerwalter.daten.KartenDatenJson
 import de.TeutonStudio.MathematikAtlas.migriereTranspositionsKnoten
+import de.TeutonStudio.MathematikKnoten.migriereTensorOperationKnoten
 import org.json.JSONObject
 
 /** App-Fassade: reiner Codec im Verwalter, App-spezifische Migration anschließend. */
 object KartenJson {
-    fun schreibe(karte: KartenDaten): String = KartenDatenJson.schreibe(karte)
+    fun schreibe(karte: KartenDaten): String = KartenDatenJson.schreibe(
+        karte.migriereTensorOperationKnoten(),
+    )
 
     fun lese(text: String): KartenDaten = lese(JSONObject(text))
 
-    fun lese(json: JSONObject): KartenDaten =
-        KartenDatenJson.lese(json).let(::migriereTranspositionsKnoten)
+    fun lese(json: JSONObject): KartenDaten = KartenDatenJson.lese(json)
+        .let(::migriereTranspositionsKnoten)
+        .migriereTensorOperationKnoten()
 }
