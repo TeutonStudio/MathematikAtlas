6c030da152a174cd616b7f0d7f0ab540b0f55e0d
Alexander <46108494+TeutonStudio@users.noreply.github.com>
2026-08-07 18:28:23 +0200

    Compose-Layoutimporte des Notiz-Knotens bereinigen

--- app/src/main/kotlin/de/TeutonStudio/MathematikAtlas/NotizKnoten.kt
+++ app/src/main/kotlin/de/TeutonStudio/MathematikAtlas/NotizKnoten.kt
@@ -1,13 +1,6 @@
 package de.TeutonStudio.MathematikAtlas
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.heightIn
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.weight
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.FilterChip
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
