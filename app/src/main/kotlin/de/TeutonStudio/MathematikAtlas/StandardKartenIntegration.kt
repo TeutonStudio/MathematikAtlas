5d16e9237a5a20b3f7e16a8f214f0cacf71ace3c
Alexander <46108494+TeutonStudio@users.noreply.github.com>
2026-08-07 19:40:34 +0200

    Standardkarten-Installer an Knotenkatalog anbinden (#332)

---
+++ app/src/main/kotlin/de/TeutonStudio/MathematikAtlas/StandardKartenIntegration.kt
@@ -0,0 +1,28 @@
+package de.TeutonStudio.MathematikAtlas
+
+import android.content.Context
+import de.TeutonStudio.MathematikAtlas.speicher.KartenSpeicher
+import de.TeutonStudio.MathematikAtlas.speicher.StandardKartenInstallationsBericht
+import de.TeutonStudio.MathematikAtlas.speicher.StandardKartenInstaller
+import de.TeutonStudio.MathematikKnoten.MathematikKnotenVorlagen
+import de.TeutonStudio.MathematikKnoten.alleMathematikKnotenVorlagen
+
+internal fun installiereStandardkarten(
+    context: Context,
+    speicher: KartenSpeicher,
+): StandardKartenInstallationsBericht {
+    val bekannteArten = buildSet {
+        alleMathematikKnotenVorlagen().mapTo(this) { it.art }
+        MengenraumKnotenVorlagen.alle.mapTo(this) { it.art }
+        GeometrieKnotenVorlagen.alle.mapTo(this) { it.art }
+        KartenWerkzeugVorlagen.alle.mapTo(this) { it.art }
+        add(MathematikKnotenVorlagen.KartenEingang.art)
+        add(MathematikKnotenVorlagen.KartenAusgang.art)
+        add("mathematik.gruppe")
+    }
+    return StandardKartenInstaller(
+        context = context,
+        speicher = speicher,
+        bekannteKnotenArten = bekannteArten,
+    ).installiere()
+}
