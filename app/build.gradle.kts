c81f17e4cb097e7edc2f85241b1f19d511811e5e
Alexander <46108494+TeutonStudio@users.noreply.github.com>
2026-08-09 16:57:57 +0200

    Temporär v2.30.0 für Master-Sync übernehmen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,8 +36,8 @@ android {
         applicationId = "de.TeutonStudio.MathematikAtlas"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2031000
-        versionName = "2.31.0"
+        versionCode = 2030000
+        versionName = "2.30.0"
         buildConfigField("String", "GIT_COMMIT_SHA", "\"$gitCommitSha\"")
     }
 
