31d158b778fbbab463691b64aa2deaeb774d800a
Alexander <46108494+TeutonStudio@users.noreply.github.com>
2026-08-09 09:25:42 +0200

    Release v2.28.5 reservieren

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,8 +36,8 @@ android {
         applicationId = "de.TeutonStudio.MathematikAtlas"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2028004
-        versionName = "2.28.4"
+        versionCode = 2028005
+        versionName = "2.28.5"
         buildConfigField("String", "GIT_COMMIT_SHA", "\"$gitCommitSha\"")
     }
 
