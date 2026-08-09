9c3cd1d4b94e74dd55b2c180a2928ca3fcc7e3ae
Alexander <46108494+TeutonStudio@users.noreply.github.com>
2026-08-09 13:27:04 +0200

    Releaseversion auf 2.29.0 klassifizieren

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,8 +36,8 @@ android {
         applicationId = "de.TeutonStudio.MathematikAtlas"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2028009
-        versionName = "2.28.9"
+        versionCode = 2029000
+        versionName = "2.29.0"
         buildConfigField("String", "GIT_COMMIT_SHA", "\"$gitCommitSha\"")
     }
 
