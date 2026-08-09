8d4abea4406b63f3f4ca384260843c2ff10132bb
Alexander <46108494+TeutonStudio@users.noreply.github.com>
2026-08-09 21:17:02 +0200

    App-KartenJson auf gemeinsamen Codec reduzieren

--- app/src/main/kotlin/de/TeutonStudio/MathematikAtlas/speicher/KartenJson.kt
+++ app/src/main/kotlin/de/TeutonStudio/MathematikAtlas/speicher/KartenJson.kt
@@ -1,30 +1,14 @@
 package de.TeutonStudio.MathematikAtlas.speicher
 
 import de.TeutonStudio.KnotenKartenVerwalter.daten.KartenDaten
-import de.TeutonStudio.KnotenKartenVerwalter.daten.KartenDatenJson
-import de.TeutonStudio.MathematikAtlas.migriereTranspositionsKnoten
-import de.TeutonStudio.MathematikKnoten.migriereDifferentialKnoten
-import de.TeutonStudio.MathematikKnoten.migriereHyperAnalysisKnoten
-import de.TeutonStudio.MathematikKnoten.migriereIntegralKnoten
-import de.TeutonStudio.MathematikKnoten.migriereTensorOperationKnoten
+import de.TeutonStudio.MathematikKnoten.MathematikKartenCodec
 import org.json.JSONObject
 
-/** App-Fassade: reiner Codec im Verwalter, App-spezifische Migration anschließend. */
+/** App-Kompatibilitätsfassade für den plattformneutralen mathematischen Karten-Codec. */
 object KartenJson {
-    fun schreibe(karte: KartenDaten): String = KartenDatenJson.schreibe(
-        karte
-            .migriereTensorOperationKnoten()
-            .migriereHyperAnalysisKnoten()
-            .migriereDifferentialKnoten()
-            .migriereIntegralKnoten(),
-    )
+    fun schreibe(karte: KartenDaten): String = MathematikKartenCodec.schreibe(karte)
 
-    fun lese(text: String): KartenDaten = lese(JSONObject(text))
+    fun lese(text: String): KartenDaten = MathematikKartenCodec.lese(text)
 
-    fun lese(json: JSONObject): KartenDaten = KartenDatenJson.lese(json)
-        .let(::migriereTranspositionsKnoten)
-        .migriereTensorOperationKnoten()
-        .migriereHyperAnalysisKnoten()
-        .migriereDifferentialKnoten()
-        .migriereIntegralKnoten()
+    fun lese(json: JSONObject): KartenDaten = MathematikKartenCodec.lese(json)
 }
