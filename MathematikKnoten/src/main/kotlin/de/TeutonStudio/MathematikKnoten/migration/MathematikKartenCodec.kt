ff26f72b1cc780a938418d5aad200237ad9e2598
Alexander <46108494+TeutonStudio@users.noreply.github.com>
2026-08-11 15:46:59 +0200

    G0.2 semantische Typmigration in Kartenpipeline aufnehmen

--- MathematikKnoten/src/main/kotlin/de/TeutonStudio/MathematikKnoten/migration/MathematikKartenCodec.kt
+++ MathematikKnoten/src/main/kotlin/de/TeutonStudio/MathematikKnoten/migration/MathematikKartenCodec.kt
@@ -46,6 +46,7 @@ object MathematikKartenMigrationen {
         .migriereDifferentialKnoten()
         .migriereIntegralKnoten()
         .migriereRechnerMethodenAnschluesse()
+        .migriereSemantischeTypen()
 
     fun nachDekodierung(karte: KartenDaten): KartenDaten = karte
         .let(::migriereTranspositionsKnoten)
@@ -53,6 +54,7 @@ object MathematikKartenMigrationen {
         .migriereHyperAnalysisKnoten()
         .migriereDifferentialKnoten()
         .migriereIntegralKnoten()
+        .migriereSemantischeTypen()
 
     fun nachLaden(karte: KartenDaten): KartenDaten = karte
         .migriereTensorraumDimensionen()
@@ -64,4 +66,5 @@ object MathematikKartenMigrationen {
         .migriereVektorRechnerKonfiguration()
         .migriereStrukturierteDivision()
         .migriereRechnerMethodenAnschluesse()
+        .migriereSemantischeTypen()
 }
