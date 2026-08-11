00558d443fb842cefad781577cd0374b129baebc
Alexander <46108494+TeutonStudio@users.noreply.github.com>
2026-08-11 15:07:20 +0200

    Godot-Typetiketten an zentrale Renderer-Dekoration anbinden

--- MathematikKnoten/src/main/kotlin/de/TeutonStudio/MathematikKnoten/AuswertungszeitKnotenRenderer.kt
+++ MathematikKnoten/src/main/kotlin/de/TeutonStudio/MathematikKnoten/AuswertungszeitKnotenRenderer.kt
@@ -8,11 +8,17 @@ import de.TeutonStudio.KnotenKartenVerwalter.schnittstelle.KnotenRenderer
 import de.TeutonStudio.KnotenKartenVerwalter.schnittstelle.KnotenRendererAktionen
 import de.TeutonStudio.MathematikKartenAdapter.KnotenAuswertungsErgebnis
 
-/** Ergänzt jeden Renderer um dieselbe Laufzeitdiagnose, ohne seine Knotendarstellung zu verändern. */
+/**
+ * Ergänzt jeden Renderer um dieselbe Laufzeitdiagnose und die aus der aktuellen
+ * mathematischen Ausgabe ableitbaren Godot-Strukturtyp-Etiketten.
+ *
+ * Beide Dekorationen sind reine Darstellung und verändern weder Knotenmodell noch
+ * Anschlussgeometrie oder Persistenz.
+ */
 fun KnotenRenderer.mitAuswertungszeit(
     ergebnisFür: (KnotenDaten) -> KnotenAuswertungsErgebnis?,
 ): KnotenRenderer {
-    val basis = this
+    val basis = this.mitGodotStrukturEtiketten(ergebnisFür)
     return object : KnotenRenderer {
         override val interaktionsModus: KnotenInteraktionsModus get() = basis.interaktionsModus
 
