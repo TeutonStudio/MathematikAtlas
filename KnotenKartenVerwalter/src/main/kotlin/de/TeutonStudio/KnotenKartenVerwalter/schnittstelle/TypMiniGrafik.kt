fc52d6c3cba522caea5ca029eb8027ae19458bde
Alexander <46108494+TeutonStudio@users.noreply.github.com>
2026-08-11 15:43:04 +0200

    G0.2 Typminigrafik Compile-Import korrigieren

--- KnotenKartenVerwalter/src/main/kotlin/de/TeutonStudio/KnotenKartenVerwalter/schnittstelle/TypMiniGrafik.kt
+++ KnotenKartenVerwalter/src/main/kotlin/de/TeutonStudio/KnotenKartenVerwalter/schnittstelle/TypMiniGrafik.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.matchParentSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
