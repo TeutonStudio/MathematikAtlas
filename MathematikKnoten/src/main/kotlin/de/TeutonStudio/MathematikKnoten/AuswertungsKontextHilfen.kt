ed2d52ebdda46f19c0c941ddf0a477bb2c6677f4
Alexander <46108494+TeutonStudio@users.noreply.github.com>
2026-08-12 14:43:08 +0200

    Add mathematical method boundary helpers

--- MathematikKnoten/src/main/kotlin/de/TeutonStudio/MathematikKnoten/AuswertungsKontextHilfen.kt
+++ MathematikKnoten/src/main/kotlin/de/TeutonStudio/MathematikKnoten/AuswertungsKontextHilfen.kt
@@ -4,8 +4,10 @@ import de.TeutonStudio.MathematikKartenAdapter.BedingterWert
 import de.TeutonStudio.MathematikKartenAdapter.KnotenAuswertungsKontext
 import de.TeutonStudio.MathematikRechenSystem.kern.AtlasWert
 import de.TeutonStudio.MathematikRechenSystem.kern.Aussage
+import de.TeutonStudio.MathematikRechenSystem.kern.MathematischeMethode
 import de.TeutonStudio.MathematikRechenSystem.kern.MathematischesObjekt
 import de.TeutonStudio.MathematikRechenSystem.kern.Methode
+import de.TeutonStudio.MathematikRechenSystem.kern.alsMathematischeMethode
 
 /**
  * Sammelt die an allen verbundenen Eingängen bereits nachgewiesenen Annahmen.
@@ -52,3 +54,12 @@ internal fun Map<String, BedingterWert>.mathematischeObjekte(rolle: String): Map
 internal fun AtlasWert.alsMethode(rolle: String): Methode =
     this as? Methode
         ?: error("$rolle benötigt eine Methode, erhielt aber ${this::class.simpleName ?: "AtlasWert"}.")
+
+internal fun AtlasWert.alsMathematischeMethode(rolle: String): MathematischeMethode =
+    alsMethode(rolle).alsMathematischeMethode(rolle)
+
+internal fun KnotenAuswertungsKontext.mathematischeMethode(
+    name: String = "methode",
+    rolle: String = "Eingang '$name'",
+): MathematischeMethode =
+    eingänge[name]?.objekt?.alsMathematischeMethode(rolle) ?: error("$rolle fehlt.")
