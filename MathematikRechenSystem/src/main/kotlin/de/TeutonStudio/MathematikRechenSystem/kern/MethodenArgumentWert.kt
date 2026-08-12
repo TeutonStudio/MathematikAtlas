e41304cda3047df6fc89e349e4eebe3d342360cb
Alexander <46108494+TeutonStudio@users.noreply.github.com>
2026-08-12 12:49:29 +0200

    MethodenArgumentWert explizit mathematisch typisieren

--- MathematikRechenSystem/src/main/kotlin/de/TeutonStudio/MathematikRechenSystem/kern/MethodenArgumentWert.kt
+++ MathematikRechenSystem/src/main/kotlin/de/TeutonStudio/MathematikRechenSystem/kern/MethodenArgumentWert.kt
@@ -1,18 +1,18 @@
 package de.TeutonStudio.MathematikRechenSystem.kern
 
 /**
- * Strukturelle Reflexionsansicht eines einzelnen Methodenarguments.
+ * Strukturelle mathematische Reflexionsansicht eines einzelnen Methodenarguments.
  *
- * Der Wert enthält bewusst die echte Parameterinstanz und ihren Wertevorrat. Damit
- * bleibt die Information beim Verdrahten erhalten und wird nicht auf einen bloßen
- * Anzeigestring reduziert.
+ * Der Wert enthält bewusst die mathematische Komponente, nicht die neutrale
+ * [MethodenKomponente]. Definitionsmengen sind eine Mathematik-Capability und keine
+ * Eigenschaft beliebiger Script-/Engine-Argumente.
  */
 data class MethodenArgumentWert(
-    val argument: MethodenArgument,
+    val argument: MathematischeArgumentKomponente,
 ) : MathematischesObjekt {
     val parameter: MethodenParameter get() = argument.parameter
-    val name: String get() = parameter.name
-    val werteVorrat: MengenAusdruck get() = argument.werteVorrat
+    val name: String get() = argument.name
+    val werteVorrat: MengenAusdruck get() = argument.definitionsMenge
 
     override fun zuLatex(): String = "${parameter.zuLatex()} \\in ${werteVorrat.zuLatex()}"
 }
