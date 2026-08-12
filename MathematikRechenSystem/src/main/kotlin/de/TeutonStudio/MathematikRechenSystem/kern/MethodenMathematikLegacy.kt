6205f893557ca3bbe1414f2220b770c96e01afca
Alexander <46108494+TeutonStudio@users.noreply.github.com>
2026-08-12 13:02:04 +0200

    Mehrdeutigen Methodenparameter-Legacyadapter entfernen

--- MathematikRechenSystem/src/main/kotlin/de/TeutonStudio/MathematikRechenSystem/kern/MethodenMathematikLegacy.kt
+++ MathematikRechenSystem/src/main/kotlin/de/TeutonStudio/MathematikRechenSystem/kern/MethodenMathematikLegacy.kt
@@ -2,15 +2,11 @@ package de.TeutonStudio.MathematikRechenSystem.kern
 
 /**
  * Zentraler Übergangsadapter für ältere Mathematikanalysen, die eine statisch als
- * [Methode] typisierte mathematische Methode wie ein mathematisches Objekt analysieren.
+ * [Methode] typisierte mathematische Methode auf freie Variablen untersuchen.
  * Neue generische Methodencodes dürfen diesen Adapter nicht verwenden.
  */
-@Deprecated("Mathematische Parameteranalyse verlangt eine explizite mathematische Methode.")
-fun Methode.enthalteneMethodenParameter(): Set<MethodenParameter> =
-    (alsMathematischeMethode("mathematische Parameteranalyse") as MathematischesObjekt)
-        .enthalteneMethodenParameter()
-
 @Deprecated("Mathematische Variablenanalyse verlangt eine explizite mathematische Methode.")
-fun Methode.enthalteneVariablen(): Set<Variable> =
-    (alsMathematischeMethode("mathematische Variablenanalyse") as MathematischesObjekt)
-        .enthalteneVariablen()
+fun Methode.enthalteneVariablen(): Set<Variable> {
+    val objekt: MathematischesObjekt = alsMathematischeMethode("mathematische Variablenanalyse")
+    return objekt.enthalteneVariablen()
+}
